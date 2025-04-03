73dcd619920b5134466cb214a028b6f6e8017f8b
124coding <124coding.github.io>
2025-04-04 03:13:05 +0900

    Visual Studio not delete

---
+++ Capstone_project/Source/Capstone_project/Capstone_projectGameModeBase.cpp
@@ -0,0 +1,5 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+
+#include "Capstone_projectGameModeBase.h"
+
---
+++ Capstone_project/Source/Capstone_project/Capstone_projectGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "Capstone_projectGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class CAPSTONE_PROJECT_API ACapstone_projectGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
